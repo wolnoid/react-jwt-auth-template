e16b94e1e19bb6c2fc0d7cb9d0488918979ba68c
Nick Choate <nickchoate@berkeley.edu>
2026-01-15 10:52:20 -0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <p>Navbar contents will go here.</p>
+    </nav>
+  );
+};
+
+export default NavBar;
